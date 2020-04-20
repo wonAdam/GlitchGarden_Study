e6183b68c0fbd7bd4a810dc24f56ddb8dc564278
AdamWon <ehdgus5500@gmail.com>
2020-04-20 16:51:44 +0900

    Day4

---
+++ Assets/Scripts/LoaderToMain.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoaderToMain : MonoBehaviour
+{
+    
+    void Start()
+    {
+        StartCoroutine(LoadToMain());
+    }
+
+    IEnumerator LoadToMain()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("Main");
+    }
+}
---
+++ Assets/Scripts/StartBtn.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartBtn : MonoBehaviour
+{
+
+
+    public void OnClick_StartBtn()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
