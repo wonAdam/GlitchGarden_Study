579d83fd5216465ceaee3f2955ceb207d3d3c1b0
AdamWon <ehdgus5500@gmail.com>
2020-04-20 19:18:22 +0900

    Day5

---
+++ Assets/Scripts/Lizard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lizard : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] Transform parentTr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        parentTr.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+
+    public void SetSpeed(float s)
+    {
+        speed = s;
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] float min = 0.2f;
+    [SerializeField] float max = 1f;
+    [SerializeField] GameObject lizard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawning());
+    }
+
+    IEnumerator Spawning()
+    {
+        while(true)
+        {
+            float sec = UnityEngine.Random.Range(min, max);
+            yield return new WaitForSeconds(sec);
+            Instantiate(lizard, transform.position, Quaternion.identity);
+        }
+    }
+}
