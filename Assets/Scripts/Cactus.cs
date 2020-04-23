63a0d04d25a19551a0398f1e5e57219d61fc1ecb
AdamWon <ehdgus5500@gmail.com>
2020-04-23 13:38:15 +0900

    Day6

---
+++ Assets/Scripts/Cactus.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cactus : MonoBehaviour
+{
+
+    [SerializeField] GameObject projectile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Shoot()
+    {
+        Instantiate(projectile, transform.position, Quaternion.identity);
+    }
+
+}
