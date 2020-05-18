6054b4fd5fc605b3bc87af4532820bfff693870a
AdamWon <ehdgus5500@gmail.com>
2020-05-18 19:54:05 +0900

    Day9

--- Assets/Scripts/Cactus.cs
+++ Assets/Scripts/Cactus.cs
@@ -7,6 +7,7 @@ public class Cactus : MonoBehaviour
 
     [SerializeField] GameObject projectile;
 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +17,26 @@ public class Cactus : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(IsEnemyInFrontOfMe())
+        {
+            //Attacker animation
+            GetComponent<Animator>().SetBool("IsAttacking", true);
+        }
+        else{
+            GetComponent<Animator>().SetBool("IsAttacking", false);
+        }
+    }
+
+    private bool IsEnemyInFrontOfMe()
+    {
+        LayerMask mask = 1 << 8;
+        RaycastHit2D hit2D = Physics2D.Raycast(transform.position, Vector2.right, 10f, mask);
+        Debug.DrawRay(transform.position, Vector2.right * 10f, Color.red);
+        if(hit2D.collider != null)
+        {
+            return true;
+        }
+        else return false;
     }
 
     public void Shoot()
